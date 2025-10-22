dd1938ddf7be338a1d02b7148698e0688f56f0b7
tien <trantienn25@gmail.com>
2025-10-22 10:43:47 +0700

    update

--- Exercises/JS/index.js
+++ Exercises/JS/index.js
@@ -2,19 +2,63 @@ const display = document.getElementById("display-screen")
 const keys= document.querySelectorAll(".btn")
 
 
-
 keys.forEach((btn)=>{
     btn.addEventListener("click", ()=>{
         const val = btn.getAttribute("data-value")
-        if(val!="="){
-            display.value+=val
+
+        switch(val){
+            case ".": {
+                if(display.value=="") return;
+                if(display.value[display.value.length-1] == ".") {
+                    display.value=""
+                    return;
+                }
+                else{
+                    display.value+=val;
+                    }
+                break;
+                }
+            case "*":
+            case "/":
+            case "+":
+            case "-": {
+                if(display.value=="") return;
+                const a = ["*", "/", "+", "-"]
+                if(a.includes(display.value[display.value.length-1])){
+                    display.value="";
+                   return;
+                }
+                else {
+                    display.value+=val
+                }
+                break;
+            }
+            case "=": {
+                try {
+                    display.value = eval(display.value);
+                   
+                }
+                catch {
+                    display.value = '';
+                    
+                }
+                break;
+            }
+            case "delete": {
+                display.value = display.value.slice(0, -1);
+               
+                break;
+            }
+            case "reset": {
+                display.value = ""
+                break
+            }
+            
+            default: {
+                display.value+=val
+            }
+            }
         }
-        else {
-            try {
-                display.value = eval(display.value);
-        } catch {
-                display.value = 'Error';
-  }
-        }
-    })
-})
+
+
+)})
